e36abcc32952bd14ef4dd6f227e7ca16a5f99c5c
lukyuvraj-lab <lukyuvraj@gmail.com>
2026-08-03 21:57:46 +0530

    Update Analytics.py

--- pages/Analytics.py
+++ pages/Analytics.py
@@ -19,6 +19,8 @@ SUPABASE_URL = "https://wkelsfwfdecgqibeolnk.supabase.co"
 
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrZWxzZndmZGVjZ3FpYmVvbG5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3NjExOTYsImV4cCI6MjEwMTMzNzE5Nn0.aNB1owMWx2ddzqe9m1iDF9w3PLE0diBTEaMzMMHBJYY"
 
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 # -----------------------------
 # TITLE
 # -----------------------------
