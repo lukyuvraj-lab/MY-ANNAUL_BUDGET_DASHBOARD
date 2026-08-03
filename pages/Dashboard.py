8fd0c156c878fabcb7cd269b1ca16c74b25eb287
lukyuvraj-lab <lukyuvraj@gmail.com>
2026-08-03 21:12:37 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
-from utils.supabase_client import supabase
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
