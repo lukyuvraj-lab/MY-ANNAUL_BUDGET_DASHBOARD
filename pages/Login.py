c1b783e441aeb20d48f0b3f98866ffd0413d9397
lukyuvraj-lab <lukyuvraj@gmail.com>
2026-08-03 22:14:08 +0530

    Update Supabase connection in Login.py
    Updated Supabase connection details with actual URL and key.

--- pages/Login.py
+++ pages/Login.py
@@ -2,8 +2,10 @@ import streamlit as st
 from supabase import create_client
 
 # Supabase Connection
-SUPABASE_URL = "YOUR_SUPABASE_URL"
-SUPABASE_KEY = "YOUR_SUPABASE_KEY"
+SUPABASE_URL = "https://wkelsfwfdecgqibeolnk.supabase.co"
+
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrZWxzZndmZGVjZ3FpYmVvbG5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3NjExOTYsImV4cCI6MjEwMTMzNzE5Nn0.aNB1owMWx2ddzqe9m1iDF9w3PLE0diBTEaMzMMHBJYY"
+
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
