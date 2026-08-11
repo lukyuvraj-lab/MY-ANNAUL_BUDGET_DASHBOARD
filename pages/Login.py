3997e69f2f3ad07e42930e31618bafc4f983351c
lukyuvraj-lab <lukyuvraj@gmail.com>
2026-08-11 18:14:55 +0530

    Update Login.py

--- pages/Login.py
+++ pages/Login.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
 from utils.supabase_client import supabase
+import os
+
+# =========================================================
+# MONEY MATE LOGO
+# =========================================================
+
+logo_path = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)),
+    "assets",
+    "moneymate_logo.png"
+)
+
+if os.path.exists(logo_path):
+    st.image(
+        logo_path,
+        width=220
+    )
 
 
 # =========================================================
